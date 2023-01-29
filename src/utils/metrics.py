38b928900da19aa3426717c8e5b4ad6338321893
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2023-01-29 16:14:23 +0100

    Update metrics.py
    Add all centrality metrics

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from typing import Dict, Optional
 
-_METRICS = ['in-deg', 'out-deg', 'betweenness', 'closeness']
+_METRICS = ['in-deg', 'out-deg', 'betweenness', 'closeness', 'pagerank', 'hits']
 
 def get_metric(network: nx.Graph, metric: str, normalize: bool = True, weight: Optional[str] = None) -> Dict[int, float]:
     assert metric in _METRICS, f'Please, use one of the followng metrics: {"; ".join(_METRICS)}'
@@ -16,6 +16,11 @@ def get_metric(network: nx.Graph, metric: str, normalize: bool = True, weight: O
         return metric_dict
     elif metric == 'closeness':
         metric_dict = nx.closeness_centrality(network, u=None, distance=weight)
+        return metric_dict
+    elif metric == 'pagerank':
+        metric_dict = nx.pagerank(network, alpha=0.85, max_iter=100, tol=1e-06, nstart=None, weight=weight, dangling=None)
+    elif metric == 'hits':
+        metric_dict = nx.hits(network, max_iter=100, tol=1e-08, nstart=None, normalized=normalize)
         return metric_dict
     else:
         raise Exception('Metric not found')
