2906dfc0b54c24259001572f9d4c96c48d487c24
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2023-02-02 14:53:28 +0100

    Update graphics.py
    Add function to plot communities

--- src/utils/graphics.py
+++ src/utils/graphics.py
@@ -39,7 +39,7 @@ def plot_centrality(gdf: gpd.GeoDataFrame, spatial_network: nx.Graph, metric_dic
     gdf.plot(ax=ax, edgecolor='darkgray', color='lightgray')
 
     coordinates = get_geodataframe_coordinates_dict(gdf)
-    node_colors = [metric_dictionary[n] if n in metric_dictionary.keys() else 0. for n in spatial_network.nodes()]
+    node_colors = [metric_dictionary[n] if n in metric_dictionary.keys() else None for n in spatial_network.nodes()]
 
     nx.draw(
         spatial_network,
@@ -51,7 +51,7 @@ def plot_centrality(gdf: gpd.GeoDataFrame, spatial_network: nx.Graph, metric_dic
         edge_color="white",
         style='--',
         alpha=.8,
-        cmap=plt.cm.YlGnBu
+        cmap=cmap
     )
 
     ax.get_xaxis().set_visible(False)
@@ -60,6 +60,42 @@ def plot_centrality(gdf: gpd.GeoDataFrame, spatial_network: nx.Graph, metric_dic
     norm = colors.Normalize(vmin=0., vmax=1.)
     fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap),
                 cax=cax)
+
+    ax.set_title(title)
+
+    plt.show()
+
+def plot_communities(gdf: gpd.GeoDataFrame, spatial_network: nx.Graph, metric_dictionary: Dict[int, str],
+                     title: str = 'Metric plot', cmap: colors.ListedColormap = plt.cm.tab10) -> None:
+    _, ax = plt.subplots(1, figsize=(15, 10))
+
+    #divider = make_axes_locatable(ax)
+    #cax = divider.append_axes('right', size='2%', pad=-.0005)
+
+    gdf.plot(ax=ax, edgecolor='darkgray', color='lightgray')
+
+    coordinates = get_geodataframe_coordinates_dict(gdf)
+    node_colors = [metric_dictionary[n] if n in metric_dictionary.keys() else None for n in spatial_network.nodes()]
+
+    nx.draw(
+        spatial_network,
+        coordinates,
+        ax=ax,
+        node_size=30,
+        width=.5,
+        node_color=node_colors,
+        edge_color="white",
+        style='--',
+        alpha=.8,
+        #cmap=cmap
+    )
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    #norm = colors.Normalize(vmin=0., vmax=1.)
+    #fig.colorbar(cm.ScalarMappable(norm=norm, cmap=cmap),
+    #            cax=cax)
 
     ax.set_title(title)
 
